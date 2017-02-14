77b2b2a3ca45085b8088d153ad062bfdbb4c9cc2
Antoine Arnoult <arnoult.antoine@gmail.com>
2017-02-14 15:05:26 +0100

    write text on right place

---
+++ src/main/groovy/com/aat/Text.groovy
@@ -0,0 +1,12 @@
+package com.aat
+
+import groovy.transform.ToString
+
+/**
+ * Created by antoine on 14/02/17.
+ */
+@ToString(includeNames=true)
+class Text {
+    String key;
+    String value;
+}
